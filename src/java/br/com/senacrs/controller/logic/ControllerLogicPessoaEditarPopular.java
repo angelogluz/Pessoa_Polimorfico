80d09e9c0c6e676a8a2185288558397404c9e42b
Angelo <Angelo@Angelo-PC>
2013-11-03 01:14:10 -0200

    Projeto de Aula

--- src/java/br/com/senacrs/controller/logic/ControllerLogicPessoaEditarPopular.java
+++ src/java/br/com/senacrs/controller/logic/ControllerLogicPessoaEditarPopular.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author Angelo
+ * @author Angelo Luz
  */
 public class ControllerLogicPessoaEditarPopular implements ControllerLogic{
 
